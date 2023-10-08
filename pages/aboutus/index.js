bdb3e62d377b5b2a4a87698eff8e57dce8c94f2e
kumaraakarshan <aakarshan0005@gmail.com>
2023-10-08 13:03:58 +0530

    final touch to about us page

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,7 +1,29 @@
-// our-domain.com/
+// pages/aboutus.js
 
-function HomePage() {
-    return <h1>The about us Page</h1>
-  }
-  
-  export default HomePage;
+import Link from 'next/link';
+
+const teamMembers = [
+  { id: 1, name: 'Yash', role: 'Senior Developer' },
+  { id: 2, name: 'Vaibhav', role: 'Backend Developer' },
+  { id: 3, name: 'Suresh', role: 'Frontend Developer' },
+];
+
+function AboutUsPage() { // Capitalized component name
+  return (
+    <div>
+      <h1>The About Us Page</h1>
+      <ul>
+        {teamMembers.map((teamMember) => (
+          <li key={teamMember.id}>
+           <Link href={`/aboutus/${teamMember.id}`}>
+  {teamMember.name}
+</Link>
+
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default AboutUsPage;
