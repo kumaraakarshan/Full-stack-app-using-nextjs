c50d6178d40798d1ccae4f4f287ed92cfe647fa7
kumaraakarshan <aakarshan0005@gmail.com>
2023-10-08 11:51:19 +0530

    about us / developer added nested routes

---
+++ pages/aboutus/developer.js
@@ -0,0 +1,7 @@
+// our-domain.com/news
+
+function developerPage() {
+    return <h1>Kumar AAkarshan</h1>
+  }
+  
+  export default developerPage;
---
+++ pages/news/something.js
@@ -0,0 +1,7 @@
+// our-domain.com/news
+
+function SomeThingPage() {
+    return <h1>The something Page</h1>
+  }
+  
+  export default SomeThingPage;
